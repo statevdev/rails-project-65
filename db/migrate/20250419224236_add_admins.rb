34709de7e638d69b3c723fa443c111a31e2c405d
statevdev <statev.dev@gmail.com>
2025-04-23 12:03:58 +0600

    try fix migration

--- db/migrate/20250419224236_add_admins.rb
+++ db/migrate/20250419224236_add_admins.rb
@@ -1,5 +1,5 @@
 class AddAdmins < ActiveRecord::Migration[7.1]
   def change
-    User.find_by(email: 'statev.dev@gmail.com').update(admin: true)
+    User.find_by(email: 'statev.dev@gmail.com')&.update(admin: true)
   end
 end
